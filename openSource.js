26142f12f2ad2ebf7d7e00877befb657bddba70e
thzero <thzero@users.noreply.github.com>
2022-12-20 07:51:41 -0600

    updates for opensource

---
+++ openSource.js
@@ -0,0 +1,39 @@
+export default () => {
+    return [
+        {
+            category: 'server',
+            name: '@thzero/library_common',
+            url: 'https://github.com/thzero/library_common',
+            licenseName: 'MIT',
+            licenseUrl: 'https://github.com/thzero/library_common/blob/master/license.md'
+        },
+        {
+            category: 'server',
+            name: '@thzero/library_common_service',
+            url: 'https://github.com/thzero/library_common_service',
+            licenseName: 'MIT',
+            licenseUrl: 'https://github.com/thzero/library_common_service/blob/master/license.md'
+        },
+        {
+            category: 'server',
+            name: '@thzero/library_service',
+            url: 'https://github.com/thzero/library_service',
+            licenseName: 'MIT',
+            licenseUrl: 'https://github.com/thzero/library_service/blob/master/license.md'
+        },
+		{
+			category: 'server',
+			name: '@thzero/library_server_repository_mongo',
+			url: 'https://github.com/thzero/library_server_repository_mongo',
+			licenseName: 'MIT',
+			licenseUrl: 'https://github.com/thzero/library_server_repository_mongo/blob/master/license.md'
+		},
+		{
+			category: 'server',
+			name: 'mongodb',
+			url: 'https://github.com/mongodb/node-mongodb-native',
+			licenseName: 'MIT',
+			licenseUrl: 'https://github.com/mongodb/node-mongodb-native/blob/master/LICENSE.md'
+		}
+    ];
+}
