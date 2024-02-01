cf75a5083a4c8f78bb3f3b4382f2ed59224c8a54
thzero <thzero@users.noreply.github.com>
2024-02-01 17:29:51 -0600

    updates

---
+++ pubSub.js
@@ -0,0 +1,51 @@
+import MongoRepository from './index.js';
+
+import NotImplementedError from '@thzero/library_common/errors/notImplemented.js';
+
+class PubSubRepository extends MongoRepository {
+	async listen(correlationId) {
+		try {
+			const collection = await this._getCollectionPubSub(correlationId);
+
+			const changeStream = collection.watch([], { fullDocument: 'updateLookup' });
+			changeStream.on('change', next => {
+				this._listen(correlationId, next.fullDocument);
+			});
+
+			return this._success(correlationId);
+		}
+		catch (err) {
+			return this._error('PubSubRepository', 'listen', null, err, null, null, correlationId);
+		}
+	}
+
+	async send(correlationId, type) {
+		const session = await this._transactionInit(correlationId, await this._getClient(correlationId));
+		try {
+			await this._transactionStart(correlationId, session);
+
+			const collection = await this._getCollectionPubSub(correlationId);
+
+			collection.insertOne({
+				type: type,
+				timestamp: new Date() // has to be a date for the Mongo TTL index to work
+			});
+
+			await this._transactionCommit(correlationId, session);
+			return this._success(correlationId);
+		}
+		catch (err) {
+			return this._error('PubSubRepository', 'send', null, err, null, null, correlationId);
+		}
+	}
+
+	async _getCollectionPubSub(correlationId) {
+		throw new NotImplementedError();
+	}
+
+	async _listen(correlationId) {
+		throw new NotImplementedError();
+	}
+}
+
+export default PubSubRepository;
