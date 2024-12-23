0b6f9490abedff2c4557191f469360cf4ae179e1
Corvette Rox <development@thzero.com>
2024-12-23 12:42:11 -0600

    moved function lower

--- collections/api.js
+++ collections/api.js
@@ -3,6 +3,10 @@ import BaseCollectionsService from './index.js';
 import NotImplementedError from '@thzero/library_common/errors/notImplemented.js';
 
 class BaseApiCollectionsService extends BaseCollectionsService {
+	getClientName() {
+		return this._config.get('db.default');
+	}
+	
 	getCollectionUsageMetrics() {
 		throw new NotImplementedError();
 	}
