752b16714a0040abe09455dd8be526fe04611ed4
Corvette Rox <development@thzero.com>
2025-07-27 18:52:50 -0500

    updates

--- baseUser.js
+++ baseUser.js
@@ -81,9 +81,7 @@ class BaseUserMongoRepository extends MongoRepository {
 			const response = this._initResponse(correlationId);
 
 			const collectionUsers = await this._getCollectionUsers(correlationId);
-			response.results = await this._findOne(correlationId, collectionUsers, { 
-				$or: [ { 'settings.gamerTag': gamerTag }, { 'settings.gamerTagUrl': gamerTag } ]
-			});
+			response.results = await this._findOne(correlationId, collectionUsers, { 'settings.gamerTag': gamerTag });
 			response.success = response.results !== null;
 	
 			if (!excludePlan && this._hasSucceeded(response) && response.results) {
